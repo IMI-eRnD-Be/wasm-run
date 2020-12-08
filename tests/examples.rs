57e10f95b3739dadf9458bb2db0af72cf603a837
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-12-08 11:21:27 +0100

    WIP

--- tests/examples.rs
+++ tests/examples.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use std::path::Path;
 use std::process::Command;
 
@@ -19,38 +18,9 @@ fn build_crate(path: &Path) {
     assert!(output.status.success());
 }
 
-fn run_crate(path: &Path, args: &[&str]) {
-    let output = Command::new("cargo")
-        .args(&["run", "--manifest-path"])
-        .arg(path.join("Cargo.toml"))
-        .arg("--")
-        .args(args)
-        .output()
-        .unwrap();
-    let stdout = String::from_utf8_lossy(&output.stdout);
-    let stderr = String::from_utf8_lossy(&output.stderr);
-
-    println!("stdout:\n{}\n", stdout);
-    eprintln!("stderr:\n{}\n", stderr);
-    assert!(output.status.success());
-}
-
 #[test]
-fn examples() {
-    let examples = std::path::PathBuf::from("examples");
+fn example_crates() {
+    let examples = Path::new("examples");
     build_crate(&examples.join("basic"));
     build_crate(&examples.join("backend-and-frontend"));
-
-    let build_path = Path::new("build");
-    let _ = fs::remove_dir_all(build_path);
-    run_crate(&examples.join("test-crate-name-vs-pkg-name"), &["build"]);
-    assert!(
-        build_path.exists(),
-        "test for `crate-name-vs-pkg-name` failed"
-    );
-
-    let build_path = examples.join("test-default-build-path").join("public");
-    let _ = fs::remove_dir_all(&build_path);
-    run_crate(&examples.join("test-default-build-path"), &["build"]);
-    assert!(build_path.exists(), "test for `default_build_path` failed");
 }
