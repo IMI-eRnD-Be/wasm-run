dd9c92af6f1ad5e40cf2e410db788b36cb017452
Cecile Tonglet <cecile.tonglet@cecton.com>
2020-12-24 10:57:54 +0100

    Build container image example (#17)

---
+++ examples/backend-and-frontend/backend/src/lib.rs
@@ -0,0 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rocket::response::NamedFile;
+use std::path::{Path, PathBuf};
+
+#[rocket::get("/")]
+pub fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("build").join("index.html")).ok()
+}
+
+#[rocket::get("/<file..>")]
+pub fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("build").join(file)).ok()
+}
+
+pub fn run() -> rocket::error::LaunchError {
+    rocket::ignite()
+        .mount("/", rocket::routes![index, files])
+        .launch()
+}
--- examples/backend-and-frontend/backend/src/main.rs
+++ examples/backend-and-frontend/backend/src/main.rs
@@ -1,24 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-use structopt::StructOpt;
-use wasm_run::prelude::*;
-use rocket::response::NamedFile;
-use std::path::{PathBuf, Path};
-
-#[rocket::get("/")]
-fn index() -> Option<NamedFile> {
-    NamedFile::open(Path::new("build").join("index.html")).ok()
-}
-
-#[rocket::get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("build").join(file)).ok()
-}
-
-#[wasm_run::main("frontend", run_server = run)]
-#[derive(StructOpt, Debug)]
-enum Cli {}
-
-fn run(_args: DefaultServeArgs) -> anyhow::Result<()> {
-    Err(rocket::ignite().mount("/", rocket::routes![index, files]).launch().into())
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Err(backend::run().into())
 }
