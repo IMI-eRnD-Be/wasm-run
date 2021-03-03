f354eab40d45486f45a160eab4ffff4d9182e118
Cecile Tonglet <cecile.tonglet@cecton.com>
2021-03-03 18:14:04 +0100

    Fix issue with run without --bin

--- examples/frontend-only/src/lib.rs
+++ examples/frontend-only/src/lib.rs
@@ -1,3 +1,7 @@
+// This is needed only of the this in wasm-run.
+// You should remove this as it prevents the autocompletion to work properly.
+#![cfg(target_arch = "wasm32")]
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
